0df0e97fd3c4331caa60ff98bd6d59f112d7e81a
Tumisang Hlaboloa <TumisangH@ragingriver.co.za>
2023-04-30 09:20:23 +0200

    More page refinements.

---
+++ Controllers/SendMailController.cs
@@ -0,0 +1,37 @@
+﻿using MobileStoreV3.Models;
+using System.Web.Mvc;
+using System.Net.Mail;
+
+namespace MobileStoreV3.Controllers
+{
+    public class SendMailController : Controller
+    {
+        // GET: SendMail
+        [HttpPost]
+        public ViewResult Index(SendMail _objModelMail)
+        {
+            if (ModelState.IsValid)
+            {
+                MailMessage mail = new MailMessage();
+                mail.To.Add(_objModelMail.To);
+                mail.From = new MailAddress(_objModelMail.From);
+                mail.Subject = _objModelMail.Subject;
+                string Body = _objModelMail.Body;
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential("username", "password"); // Enter seders User name and password  
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+                return View("Index", _objModelMail);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
